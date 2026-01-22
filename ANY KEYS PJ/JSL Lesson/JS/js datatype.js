160b6ed7237b60e95eeea4ade33cb6e840e31658
kaythiaung555 <roijilahtaw777@gmail.com>
2026-01-22 19:57:25 +0630

    yes

---
+++ ANY KEYS PJ/JSL Lesson/JS/js datatype.js
@@ -0,0 +1,47 @@
+// String Data Type
+
+let name = "Kay Thi Aung"
+console.log(name);
+console.log(typeof(name));
+
+// numeric data type (number data type)
+let age = 29;
+console.log(age);
+console.log(typeof(age));
+
+// boolean data type  0 fale, 1 true
+let single = true;
+console.log(single);
+console.log(typeof(single));
+
+// undifined 
+let myBaby;
+console.log(myBaby);
+
+// null
+let son = null;
+console.log(son);
+
+// array
+let arr1 = new Array('a', 'b','c');
+console.log(arr1)
+console.log(typeof(arr1));
+
+let arr2 = [1,2,3,4,5]
+console.log(arr2);
+console.log(typeof(arr2));
+
+// multiarry 
+let multi = ['Mg Mg', 'Aung Aung', 'Hla Hla',['Tun Tun', 'Mya Mya'],'Aye Aye'];
+console.log(multi);
+console.log(typeof(multi));
+
+// Object
+let student = {
+    // key : value ,
+    name : 'Kyaw Kyaw', 
+    age : 27,
+    address : "Yangon",
+    education : "B.C.Sc",
+}
+console.log(student);
